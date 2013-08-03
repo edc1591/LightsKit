df91cfe1cb6ef7af5e8287e9f748bbcb928a9c16
Evan Coleman <evan@evancoleman.net>
2013-08-02 23:22:53 -0400

    Initial Commit

---
+++ LightsKit/LightsKit.h
@@ -0,0 +1,13 @@
+//
+//  LightsKit.h
+//  LightsKit
+//
+//  Created by Evan Coleman on 8/2/13.
+//  Copyright (c) 2013 Evan Coleman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LightsKit : NSObject
+
+@end
---
+++ LightsKit/LightsKit.m
@@ -0,0 +1,13 @@
+//
+//  LightsKit.m
+//  LightsKit
+//
+//  Created by Evan Coleman on 8/2/13.
+//  Copyright (c) 2013 Evan Coleman. All rights reserved.
+//
+
+#import "LightsKit.h"
+
+@implementation LightsKit
+
+@end
